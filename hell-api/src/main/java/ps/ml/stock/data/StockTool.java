681b50867d8c8b67a62ebc593eff4c02f7142af7
jiang.xu <jiang.xu@duitang.com>
2016-09-30 02:22:34 +0800

    add get all stock codes .

--- hell-api/src/main/java/ps/ml/stock/data/StockTool.java
+++ hell-api/src/main/java/ps/ml/stock/data/StockTool.java
@@ -1,5 +1,11 @@
 package ps.ml.stock.data;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,8 +21,27 @@ public class StockTool {
 
 
     public List<StockBean> getUsedStocks(){
+    
+        try {
+            URL url = new URL("http://218.244.146.57/static/all.csv");
+            HttpURLConnection uc = (HttpURLConnection) url.openConnection();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(),
+                "GBK"));
+            String line;
+            while((line = reader.readLine()) != null){
+                System.out.println(line.split(",")[0]);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
 //        FileUtil2 =
         return null;
     }
-
+    
+    public static void main(String[] args){
+        StockTool stockTool = new StockTool();
+        stockTool.getUsedStocks();
+    }
 }
